e2ca8a1a24755dfc32646bb6fa7ea1f8aa933def
konarsada <sadabratak@gmail.com>
2022-04-23 23:18:20 +0530

    Heap Sort

--- heapSort.py
+++ heapSort.py
@@ -1,7 +1,6 @@
 def maxHeapify(arr, i):
     l = 2 * i + 1
     r = 2 * i + 2
-    
     largest = i
     
     if(l < len(arr) and arr[l] > arr[largest]):
@@ -10,7 +9,7 @@ def maxHeapify(arr, i):
     if(r < len(arr) and arr[r] > arr[largest]):
         largest = r
     
-    if(i != largest):
+    if(largest != i):
         arr[i], arr[largest] = arr[largest], arr[i]
         maxHeapify(arr, largest)
 
@@ -18,7 +17,14 @@ def buildMaxHeap(arr):
     for i in range(len(arr) // 2 - 1, -1, -1):
         maxHeapify(arr, i)
 
+def heapSort(arr):
+    buildMaxHeap(arr)
+    
+    for i in range(len(arr) - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        maxHeapify(arr, 0)
+
 arr = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]
 print(arr)
-buildMaxHeap(arr)
+heapSort(arr)
 print(arr)
