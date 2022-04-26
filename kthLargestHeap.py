5008a6fe047b6e9b2c79bf84aaae75f07d3eb351
konarsada <sadabratak@gmail.com>
2022-04-26 23:45:23 +0530

    kth largest using heap

--- kthLargestHeap.py
+++ kthLargestHeap.py
@@ -1,11 +1,11 @@
 def maxHeapify(arr, i, n):
     l = 2 * i + 1
     r = 2 * i + 2
+    
     largest = i
     
     if(l < n and arr[l] > arr[largest]):
         largest = l
-    
     if(r < n and arr[r] > arr[largest]):
         largest = r
     
@@ -15,22 +15,23 @@ def maxHeapify(arr, i, n):
 
 def buildMaxHeap(arr):
     heapSize = len(arr)
-    
     for i in range(heapSize // 2 - 1, -1, -1):
         maxHeapify(arr, i, heapSize)
 
-def heapSort(arr):
+def getKthMax(arr, k):
     buildMaxHeap(arr)
     
     heapSize = len(arr)
+    countK = 0
     
     for i in range(len(arr) - 1, 0, -1):
+        countK += 1
+        if(countK == k):
+            return arr[0]
         arr[i], arr[0] = arr[0], arr[i]
         heapSize -= 1
         maxHeapify(arr, 0, heapSize)
 
 arr = [7, 10, 4, 3, 20, 15]
-#arr = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]
 print(arr)
-heapSort(arr)
-print(arr)
+print(getKthMax(arr, 4))
--- maxHeap.py
+++ maxHeap.py
@@ -18,7 +18,8 @@ def buildMaxHeap(arr):
     for i in range(len(arr) // 2 - 1, -1, -1):
         maxHeapify(arr, i)
 
-arr = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]
+# arr = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]
+arr = [7, 10, 4, 3, 20, 15]
 print(arr)
 buildMaxHeap(arr)
 print(arr)
